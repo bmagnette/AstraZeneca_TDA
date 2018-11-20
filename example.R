3716fef414c9466239052e39b8e1ff9fec44c520
Baptiste <baptiste.magnette@gmail.com>
2018-11-20 14:09:40 +0100

    First commit

---
+++ example.R
@@ -0,0 +1,11 @@
+# install.packages("TDA")
+# https://cran.r-project.org/web/packages/TDA/vignettes/article.pdf
+
+library(TDA)
+
+# Distance functions and density estimators
+
+
+# Density clustering example
+
+# Persistant homology example
