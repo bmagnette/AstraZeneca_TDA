de7225e6266714982d79ecc1d8fbb677df6c69db
Baptiste <baptiste.magnette@gmail.com>
2018-11-22 10:11:41 +0100

    Adding example data and loading it.

--- R/example.R
+++ R/example.R
@@ -1,10 +1,14 @@
 # install.packages("TDA")
-# https://cran.r-project.org/web/packages/TDA/vignettes/article.pdf
+# install.packages("devtools")
+#devtools::install_github("paultpearson/TDAmapper")
 
+library(devtools)
 library(TDA)
+library(TDAmapper)
+
+data <- read.table("data/Iris.csv", header=TRUE, sep=",", dec=".", quote = "\"", na.strings = "NA")
 
 # Distance functions and density estimators
-
 
 # Density clustering example
 
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Astra Zeneca Project
 
-- Roadmap
+### Support software
+Trello : https://trello.com/b/jSLJXYzT/Theory
+Github : https://github.com/bmagnette/AstraZeneca_TDA
+
+### Library help 
+TDA Mapper : https://github.com/paultpearson/TDAmapper
+TDA (main packages): https://cran.r-project.org/web/packages/TDA/vignettes/article.pdf
+
+### Lecture 
+Ayasdi : https://www.ayasdi.com/blog/category/topology/
